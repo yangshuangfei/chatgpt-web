a698578f6d3badd66cfa11732767524244b11eed
yangsf1 <yangsf1@qiaqiafood.com>
2023-03-23 22:47:13 +0800

    修改

--- service/src/index.ts
+++ service/src/index.ts
@@ -13,6 +13,7 @@ import { limiter } from './middleware/limiter'
 import { isNotEmptyString } from './utils/is'
 import { minxin } from './config'
 import { access_token } from './utils/access_token'
+const cors = require("cors")
 
 const secretKey = 'abdbdodjod-^-^-'
 const app = express()
@@ -21,23 +22,26 @@ const router = express.Router()
 app.use(logger('dev'))
 app.use(express.static('public'))
 app.use(express.json())
+app.use(cors())
 app.use(
-  expressjwt({ secret: secretKey, algorithms: ['HS256'] }).unless({ path: ['/session', '/login'] }),
+  expressjwt({ secret: secretKey, algorithms: ['HS256'] }).unless({ path: ['/api/session', '/api/login'] }),
 ) // /匹配的内容/ ^不在\转义/api/
 
-app.all('*', (_, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Authorization, Content-Type')
-  res.header('Access-Control-Allow-Methods', '*')
-  res.setHeader('Access-Control-Allow-Headers', 'Authorization')
-  next()
-})
+app.all('*', function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  // res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("X-Powered-By", ' 3.2.1')
+  if (req.method === "OPTIONS") res.send(200);/*让options请求快速返回*/
+  else next();
+});
 
 router.post('/chat-process', [limiter], async (req, res) => {
   res.setHeader('Content-type', 'application/octet-stream')
 
   try {
-    debug('123333', req)
     const { prompt, options = {}, systemMessage } = req.body as RequestProps
     let firstChunk = true
     await chatReplyProcess({
@@ -100,24 +104,20 @@ router.post('/login', async (req, res) => {
     const { code } = req.body as { code: string }
     if (!code)
       throw new Error('Secret key is empty')
+    var userid: any
     // 用户信息对象 密钥 有效期
-    let userid: string = null
-    access_token(async (data) => {
-      if (data) {
-        const url = minxin.url
-        // 根据access_token和用户code获取用户基本信息
-        const response1 = await axios.get(`${url}auth/getuserinfo?access_token=${data}&code=${code}`)
-        userid = response1.data.userid
-      }
+    access_token(async (data: any) => {
+      // 根据access_token和用户code获取用户基本信息
+      const url = minxin.url
+      const response = await axios.get(`${url}auth/getuserinfo?access_token=${data}&code=${code}`)
+      userid = response.data.userid
+      if (userid == null || userid === undefined)
+        throw new Error('获取用户信息失败')
+      const tokenStr = jwt.sign(
+        { username: userid }, secretKey, { expiresIn: '2h' },
+      )
+      res.send({ status: 'Success', message: 'Verify successfully', data: tokenStr })
     })
-
-    if (userid == null || userid === undefined)
-      throw new Error('获取用户信息失败')
-
-    const tokenStr = jwt.sign(
-      { username: userid }, secretKey, { expiresIn: '2h' },
-    )
-    res.send({ status: 'Success', message: 'Verify successfully', data: tokenStr })
   }
   catch (error) {
     res.send({ status: 'Fail', message: error.message, data: null })
@@ -127,4 +127,4 @@ router.post('/login', async (req, res) => {
 app.use('', router)
 app.use('/api', router)
 
-app.listen(3002, () => globalThis.console.log('Server is running on port 3002'))
+app.listen(19038, () => globalThis.console.log('Server is running on port 19038'))
